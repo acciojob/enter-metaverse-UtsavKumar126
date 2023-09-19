eab63d40f93964ef1793e993125225e847d68a80
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2023-09-19 17:58:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const buttons=document.getElementById("enterBtn");
+
+document.getElementById("enterBtn").addEventListener("click", func);
+
+
+function  func() {
+	const para=document.getElementById("status");
+	para.remove();
+
+	const headingTag=document.createElement("h1");
+	headingTag.innerText="Entered Metaverse";
+
+	const bodyTag=document.getElementsByTagName("BODY")[0];
+	bodyTag.insertBefore(headingTag,buttons);	
+}
